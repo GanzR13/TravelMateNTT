da4ff53a381da2851f436e524468be8c74325346
M.ARIFANDI PRASETYO <m.arifandiprasetyo354@gail.com>
2024-12-07 09:55:09 +0700

    FrondEnd

--- app/src/main/java/com/example/travelmatentt/Detail/Detail.kt
+++ app/src/main/java/com/example/travelmatentt/Detail/Detail.kt
@@ -19,7 +19,7 @@ class Detail : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_detail)
 
-        // Setting up padding to avoid system bars
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
