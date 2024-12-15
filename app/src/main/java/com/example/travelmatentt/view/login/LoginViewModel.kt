7477cff6cc46ada97d99fcb8ebd51cd30d7a21a2
GanzR13 <rizkimubin13@gmail.com>
2024-12-15 16:38:03 +0700

    Setting User

--- app/src/main/java/com/example/travelmatentt/view/login/LoginViewModel.kt
+++ app/src/main/java/com/example/travelmatentt/view/login/LoginViewModel.kt
@@ -1,15 +1,44 @@
 package com.example.travelmatentt.view.login
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.travelmatentt.data.preference.UserModel
-import com.example.travelmatentt.data.repository.Repository
+import com.example.travelmatentt.data.request.LoginRequest
+import com.example.travelmatentt.data.retrofit.ApiConfig
 import kotlinx.coroutines.launch
 
-class LoginViewModel(private val repository: Repository) : ViewModel() {
-    fun login(email: String, password: String) = repository.login(email, password)
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
-    fun saveSession(userModel: UserModel) = viewModelScope.launch {
-        repository.saveSession(userModel)
+    private val _loginStatus = MutableLiveData<Boolean>()
+    val loginStatus: LiveData<Boolean> get() = _loginStatus
+
+    private val sharedPreferences = application.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+
+    fun loginUser(email: String, password: String) {
+        viewModelScope.launch {
+            try {
+                val loginRequest = LoginRequest(email, password)
+                val response = ApiConfig.getApiService().login(loginRequest)
+
+                if (!response.accessToken.isNullOrEmpty()) {
+                    saveTokens(response.accessToken, response.refreshToken)
+                    _loginStatus.postValue(true)
+                } else {
+                    _loginStatus.postValue(false)
+                }
+            } catch (e: Exception) {
+                _loginStatus.postValue(false)
+            }
+        }
+    }
+
+    private fun saveTokens(accessToken: String, refreshToken: String?) {
+        val editor = sharedPreferences.edit()
+        editor.putString("access_token", accessToken)
+        editor.putString("refresh_token", refreshToken)
+        editor.apply()
     }
 }
